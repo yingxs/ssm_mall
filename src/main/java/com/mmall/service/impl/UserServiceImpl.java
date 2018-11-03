09092cc32d97a081553dd5f29c391d26fbe454cb
王昊 <yingxs888@126.com>
2018-11-03 17:27:30 +0800

    feat (User):登出，注册，校验接口的定义与开发

--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.mmall.service.impl;
 
+import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
 import com.mmall.dao.UserMapper;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import com.mmall.util.MD5Util;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,7 +28,9 @@ public class UserServiceImpl implements IUserService {
             return ServerResponse.createByErrorMessage("用户名不存在");
         }
 
-        User user = userMapper.selectLogin(username, password);
+        //todo MD5
+        String md5Password = MD5Util.MD5EncodeUtf8(password);
+        User user = userMapper.selectLogin(username, md5Password);
         if(user == null){
             return ServerResponse.createByErrorMessage("密码错误");
         }
@@ -34,6 +38,51 @@ public class UserServiceImpl implements IUserService {
         user.setPassword(StringUtils.EMPTY);
 
         return ServerResponse.createBySuccess("登陆成功",user);
+    }
+
+    public ServerResponse<String> register(User user){
+        ServerResponse vaildResponse = this.checkValid(user.getUsername(),Const.USERNAME);
+        if(!vaildResponse.isSuccess()){
+            return vaildResponse;
+        }
+
+        vaildResponse = this.checkValid(user.getEmail(),Const.EMAIL);
+        if(!vaildResponse.isSuccess()){
+            return vaildResponse;
+        }
+
+        user.setRole(Const.Role.ROLE_CUSTOMER);
+        //MD5加密
+        user.setPassword(MD5Util.MD5EncodeUtf8(user.getPassword()));
+        int resultCount = userMapper.insert(user);
+        if(resultCount == 0){
+            return ServerResponse.createByErrorMessage("注册失败");
+        }
+        return ServerResponse.createBySuccessMessage("注册成功");
+    }
+
+
+    public ServerResponse<String> checkValid(String str,String type){
+        if(StringUtils.isNoneBlank(type)){
+            //开始校验
+            if(Const.USERNAME.equals(type)){
+                int resultCount = userMapper.checkEmail(str);
+                if(resultCount > 0){
+                    return ServerResponse.createByErrorMessage("用户名已存在");
+                }
+            }
+
+            if(Const.EMAIL.equals(type)){
+                int resultCount = userMapper.checkEmail(str);
+                if(resultCount > 0){
+                    return ServerResponse.createByErrorMessage("email已存在");
+                }
+            }
+        }else{
+            return ServerResponse.createByErrorMessage("参数错误");
+        }
+
+        return ServerResponse.createBySuccessMessage("校验成功");
     }
 
 
