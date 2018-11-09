ea788d682df21965a53911a1e367e7f03cab2cf8
王昊 <yingxs888@126.com>
2018-11-09 16:29:59 +0800

    feat (Cart):购物车的添加功能

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -17,6 +17,14 @@ public class Const {
         Set<String> PROCE_ASC_DESC =  Sets.newHashSet("price_desc","price_asc");
     }
 
+    public interface Cart{
+        int CHECKED = 1;//即购物车选中状态
+        int UN_CHECKED = 0;//即购物车中未选中状态
+
+        String LIMIT_NUM_FAIL = "LIMIT_NUM_FAIL";
+        String LIMIT_NUM_SUCCESS = "LIMIT_NUM_SUCCESS";
+    }
+
 
 
     public interface Role{
---
+++ src/main/java/com/mmall/service/ICartService.java
@@ -0,0 +1,12 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.vo.CartVo;
+
+/**
+ * Created by admin on 2018/11/9.
+ */
+public interface ICartService {
+
+    ServerResponse<CartVo> add(Integer userId, Integer productId, Integer count);
+}
