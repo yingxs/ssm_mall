0f8df3b13a8accfea000f6b57a0304137d7a24cb
王昊 <yingxs888@126.com>
2018-12-24 11:51:04 +0800

    ref (TokenCache):用户重置密码Token迁移到Redis

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -13,6 +13,7 @@ public class Const {
     public static final String CURRENT_USER = "current_User";
     public static final String EMAIL = "email";
     public static final String USERNAME = "username";
+    public static final String TOKEN_PREFIX = "token_";
 
     public interface ProductListOrderBy{
         Set<String> PROCE_ASC_DESC =  Sets.newHashSet("price_desc","price_asc");
