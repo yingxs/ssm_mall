83c3d45986accb0d470735a36eb30532b6fc1402
王昊 <yingxs888@126.com>
2018-12-28 23:12:51 +0800

    feat (admin):管理员权限校验初步

---
+++ src/main/java/com/mmall/controller/common/interceptor/AuthorityInterceptor.java
@@ -0,0 +1,92 @@
+package com.mmall.controller.common.interceptor;
+
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisShardedPoolUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
+
+@Slf4j
+public class AuthorityInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //请求中Controller中的方法
+        HandlerMethod handlerMethod = (HandlerMethod)handler;
+
+        //解析HandlerMethod
+        String methodName = handlerMethod.getMethod().getName();
+        String className = handlerMethod.getBean().getClass().getSimpleName();
+        
+        
+        //解析参数
+        StringBuffer requestParamBuffer = new StringBuffer();
+        Map<String, String[]> paramMap = request.getParameterMap();
+        Iterator<Map.Entry<String, String[]>> it = paramMap.entrySet().iterator();
+        while (it.hasNext()){
+            Map.Entry entry = it.next();
+            String mapKey = (String)entry.getKey();
+            String mapValue = StringUtils.EMPTY;
+
+            Object obj = entry.getValue();
+            if(obj instanceof String[]){
+                String[] strs = (String[]) obj;
+                mapValue = Arrays.toString(strs);
+            }
+
+            requestParamBuffer.append(mapKey).append("=").append(mapValue).append(" ");
+
+        }
+
+        User user = null;
+        String loginToken = CookieUtil.readLoginToken(request);
+        if(StringUtils.isNotEmpty(loginToken)){
+            String userJsonStr = RedisShardedPoolUtil.get(loginToken);
+            user = JsonUtil.string2Obj(userJsonStr, User.class);
+        }
+
+        if(user == null || (user.getRole() != Const.Role.ROLE_ADMIN)){
+            //返回false
+            response.reset();//这里要添加reset方法，否则会报异常getWriter() has ...
+            response.setCharacterEncoding("UTF-8");//设置编码 防止乱码
+            response.setContentType("application/json;charset=UTF-8");//设置返回值类型
+            PrintWriter out = response.getWriter();
+
+            if(user == null){
+                out.print(JsonUtil.obj2String(ServerResponse.createByErrorMessage("拦截器拦截，用户未登录")));
+            }else{
+                out.print(JsonUtil.obj2String(ServerResponse.createByErrorMessage("拦截器拦截，用户无权限操作")));
+            }
+            out.flush();
+            out.close();
+            return false;//不进入Controller
+        }
+
+
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
